b98d4edc447bbf8395addd5c67175bb032ee9e42
mustafa <shamohammadi64@gmail.com>
2025-02-26 16:04:08 +0330

    package

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -2,7 +2,9 @@
 function NavBar() {
   return (
     <div className="h-24 flex justify-center items-center font-bold">
-      <h2>NavBar</h2>
+      <h2><li>home</li>
+      <li>gallery</li>
+      <li>product</li></h2>
     </div>
   );
 }
