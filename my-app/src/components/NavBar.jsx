65077e3b1bbf61af7650158802f4858a420efd76
mustafa <shamohammadi64@gmail.com>
2025-02-26 16:16:31 +0330

    navbar

--- my-app/src/components/NavBar.jsx
+++ my-app/src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 
 function NavBar() {
   return (
-    <div className="h-24 flex justify-center items-center font-bold">
-      <h2><li>home</li>
+    <div className="h-24 list-none flex justify-center items-center font-bold">
+      <h2 className="[&>*]:text-gray-700 [&>*]:cursor-pointer flex justify-evenly w-10/12 border h-10 items-center "><li>home</li>
       <li>gallery</li>
       <li>product</li></h2>
     </div>
